7a5238047f09edea75c6e584dd84251bec3a4c63
Justin Tanacredi <jtanacredi@gmail.com>
2015-10-07 17:43:47 -0400

    Add additional content component. Convert css files to sass.

--- both/pages/ThinksCode.jsx
+++ both/pages/ThinksCode.jsx
@@ -1,7 +1,9 @@
 ThinksCode = React.createClass({
   render() {
     return (
+      <section className={classNames('fullPageSection', 'codeSection')}>
       <div>Justin thinks he can code</div>
+      </section>
     );
   }
 });
---
+++ both/pages/ThinksSkate.jsx
@@ -0,0 +1,9 @@
+ThinksSkate = React.createClass({
+  render() {
+    return (
+      <section className={classNames('fullPageSection', 'skateSection')}>
+      <div>Justin thinks he can skate</div>
+      </section>
+    );
+  }
+});
